856801d0e0f19e5ae304e264e5a2fa293d7d534f
will <william@rigney.com.au>
2024-09-06 22:18:11 +1000

    format crates.nvim settings + minor language changes

--- .config/nvim/lua/plugins/crates.lua
+++ .config/nvim/lua/plugins/crates.lua
@@ -5,67 +5,65 @@
 return {
 	'saecki/crates.nvim',
 	event = { "BufRead Cargo.toml" },
-	config = function()
-		require('crates').setup {
-			completion = {
-				cmp = {
-					enabled = true,
-				},
-				crates = {
-					enabled = true, -- disabled by default
-					max_results = 8, -- The maximum number of search results to display
-					min_chars = 3 -- The minimum number of charaters to type before completions begin appearing
-				},
-				-- disable icons in completions
-				text = {
-					prerelease = " pre-release ",
-					yanked = " yanked ",
-				},
-
-			},
-			lsp = {
+	opts = {
+		completion = {
+			cmp = {
 				enabled = true,
-				actions = true,
-				completion = true,
-				hover = true,
+			},
+			crates = {
+				enabled = true, -- disabled by default
+				max_results = 8, -- The maximum number of search results to display
+				min_chars = 2 -- The minimum number of charaters to type before completions begin appearing
+			},
+			-- disable icons in completions
+			text = {
+				prerelease = " pre-release ",
+				yanked = " yanked ",
 			},
 
-			-- following is to totally disable icons
+		},
+		lsp = {
+			enabled = true,
+			actions = true,
+			completion = true,
+			hover = true,
+		},
+
+		-- following is to totally disable icons
+		text = {
+			loading = "  loading...",
+			version = "  %s",
+			prerelease = "  %s",
+			yanked = "  %s yanked",
+			nomatch = "  not found",
+			upgrade = "  %s",
+			error = "  error fetching crate",
+		},
+		popup = {
 			text = {
-				loading = "  Loading...",
-				version = "  %s",
-				prerelease = "  %s",
-				yanked = "  %s yanked",
-				nomatch = "  Not found",
-				upgrade = "  %s",
-				error = "  Error fetching crate",
+				title = "# %s",
+				pill_left = "",
+				pill_right = "",
+				created_label = "created        ",
+				updated_label = "updated        ",
+				downloads_label = "downloads      ",
+				homepage_label = "homepage       ",
+				repository_label = "repository     ",
+				documentation_label = "documentation  ",
+				crates_io_label = "crates.io      ",
+				categories_label = "categories     ",
+				keywords_label = "keywords       ",
+				version = "%s",
+				prerelease = "%s pre-release",
+				yanked = "%s yanked",
+				enabled = "* s",
+				transitive = "~ s",
+				normal_dependencies_title = "  dependencies",
+				build_dependencies_title = "  build dependencies",
+				dev_dependencies_title = "  dev dependencies",
+				optional = "? %s",
+				loading = " ...",
 			},
-			popup = {
-				text = {
-					title = "# %s",
-					pill_left = "",
-					pill_right = "",
-					created_label = "created        ",
-					updated_label = "updated        ",
-					downloads_label = "downloads      ",
-					homepage_label = "homepage       ",
-					repository_label = "repository     ",
-					documentation_label = "documentation  ",
-					crates_io_label = "crates.io      ",
-					categories_label = "categories     ",
-					keywords_label = "keywords       ",
-					version = "%s",
-					prerelease = "%s pre-release",
-					yanked = "%s yanked",
-					enabled = "* s",
-					transitive = "~ s",
-					normal_dependencies_title = "  Dependencies",
-					build_dependencies_title = "  Build dependencies",
-					dev_dependencies_title = "  Dev dependencies",
-					optional = "? %s",
-					loading = " ...",
-				},
-			},
-		}
-	end,
+		},
+	}
 }
