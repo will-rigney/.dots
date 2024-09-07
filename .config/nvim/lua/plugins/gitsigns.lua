e9f55230d2de3e5c22668118fc9120998324e617
will <william@rigney.com.au>
2024-09-07 17:18:11 +1000

    update gitsigns config format

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -3,8 +3,10 @@
 -- https://github.com/lewis6991/gitsigns.nvim
 
 return {
-	"lewis6991/gitsigns.nvim",
-	config = function ()
-		require 'gitsigns'.setup()
-	end
+	'lewis6991/gitsigns.nvim',
+	opts = {
+		preview_config = {
+			border = 'single',
+		},
+	},
 }
