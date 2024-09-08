54c8121b62cfda9ce04021945c8e5ea5b0a3b9ed
will <william@rigney.com.au>
2024-09-08 18:38:47 +1000

    oil config inc custom keybind descriptions

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -4,19 +4,47 @@
 
 return {
 	'stevearc/oil.nvim',
-	-- ---@module 'oil'
-	-- ---@type oil.SetupOpts
+	---@module 'oil'
+	---@type oil.SetupOpts
 	opts = {
+		skip_confirm_for_simple_edits = true,
 		default_file_explorer = true,
 		watch_for_changes = true,
 		constrain_cursor = 'name',
+		use_default_keymaps = false, -- copied below, n.b. might breaking change one day
+		keymaps = {
+			['g?'] = 'actions.show_help',
+			['<CR>'] = 'actions.select',
+			-- <C-s> & <C-h> is wrong, should be same as bindings elsewhere
+			['<C-s>'] = { 'actions.select', opts = { vertical = true }, desc = 'open the entry in a vertical split' },
+			['<C-h>'] = { 'actions.select', opts = { horizontal = true }, desc = 'open entry in a horizontal split' },
+			['<C-t>'] = { 'actions.select', opts = { tab = true }, desc = 'open entry in new tab' },
+			['<C-p>'] = { 'actions.preview', desc = 'open preview' }, -- not sure about this bindings
+			['<C-c>'] = { 'actions.close', desc = 'close oil' },
+			['<C-l>'] = { 'actions.refresh', desc = 'refresh oil' }, -- should need to do as watching is enabled
+			['-'] = { 'actions.parent', desc = 'open parent directory' },
+			['_'] = { 'actions.open_cwd', desc = 'open currend working directory' },
+			['`'] = { 'actions.cd', desc = 'cd here' },
+			['~'] = { 'actions.cd', opts = { scope = 'tab' }, desc = ':tcd to the current oil directory' },
+			['gs'] = 'actions.change_sort',
+			['gx'] = 'actions.open_external',
+			['g.'] = 'actions.toggle_hidden',
+			['g\\'] = 'actions.toggle_trash',
+		},
 		view_options = {
 			show_hidden = true,
+			natural_order = true, -- false more machine like & performant
 		},
 		columns = {
 			'permissions',
 			'size',
 			'mtime',
+		},
+		preview = {
+			border = 'none',
+		},
+		progress = {
+			border = 'none',
 		},
 		-- floating entry window
 		float = {
