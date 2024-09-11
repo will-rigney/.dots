1f493989f18dfc81937a8c74d6827530e358c0ab
will <william@rigney.com.au>
2024-09-11 12:32:04 +1000

    lazy config disable icons

--- .config/nvim/lua/lazyinit.lua
+++ .config/nvim/lua/lazyinit.lua
@@ -1,5 +1,5 @@
--- using lazy.nvim for plugin package management
--- pls minimise number of packages to bare minimum
+-- lazy.nvim for plugin package management
+-- pls minimise number of packages
 
 -- bootstrap lazy
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
@@ -21,9 +21,45 @@ vim.opt.rtp:prepend(lazypath)
 
 -- setup lazy
 require('lazy').setup {
+	-- disable icons in the ui
 	spec = {
 		{ import = 'plugins' },
+		-- what else do we import if anything
 	},
-	-- automatically check for plugin updates
-	checker = { enabled = true },
+    -- automatically check for plugin updates
+	checker = {
+		enabled = true,
+	},
+	-- install = {
+	-- 	colorscheme = 'default',
+	-- },
+	ui = {
+		icons = {
+			-- useful to know
+			cmd = 'cmd:',
+			config = 'cfg:',
+			event = 'event:',
+			favorite = 'favourite',
+			ft = 'ft:',
+			init = 'init:',
+			import = 'import: ',
+			keys = 'keys: ',
+			lazy = 'lazy',
+			-- still icons
+			loaded = '●',
+			not_loaded = '○',
+			-- skip for these
+			plugin = '',
+			runtime = '',
+			-- ^ for start
+			start = '^',
+			-- explicit
+			source = 'source ',
+			require = 'require ',
+			-- what is a task? a face?
+			task = require('decor').get_face(),
+			-- list nesting styles (no icons)
+			list = { '*', '>', '-', '‒' },
+		},
+	},
 }
