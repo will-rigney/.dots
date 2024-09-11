da2e28a1b6062d785e598bd2ecf630a164afe7c6
will <william@rigney.com.au>
2024-09-11 14:34:16 +1000

    clangd exclusive switch header binding

--- .config/nvim/lua/plugins/clangd-extensions.lua
+++ .config/nvim/lua/plugins/clangd-extensions.lua
@@ -7,4 +7,8 @@ return {
 	dependencies = {
 		{ 'neovim/nvim-lspconfig' },
 	},
+	config = function()
+		-- clang exclusive, n.b. binding should work for sourcekit / objective-c too
+		vim.keymap.set('n', '<leader>ch', '<cmd>ClangdSwitchSourceHeader<cr>', { desc = '[c]ode switch source [h]eader' })
+	end,
 }
