1f370cb43557026d57716390fe05a5cfa5e4a1b0
will <william@rigney.com.au>
2024-09-11 13:31:32 +1000

    update + autofmt init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,25 +5,34 @@
 -- remember to use :h for help text on any option
 
 -- env
-vim.env.BASH_ENV = "$HOME/.bashrc" -- use bash env in vim env (aliases etc)
 
 -- filetype
-vim.cmd "filetype plugin indent on" -- enables plugin & indent files
+vim.cmd 'filetype plugin on' -- enables plugin files only
+-- vim.cmd 'filetype plugin indent on' -- enables plugin & indent files (probably enabled by default
 
--- map leader
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
 
--- providers
+-- disable providers
 vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_python3_provider = 0
 vim.g.loaded_ruby_provider = 0
--- :checkhealth for more information
+-- :checkhealth to confirm
+
+-- map leader
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
 --- modules
-require "options"
-require "autocmd"
-require "lazyinit"
-require "statusline"
-require "keymap"
+require 'options'
+require 'statusline'
+require 'lazyinit'
+require 'autocmd'
+require 'keymap'
+
+-- color scheme
+-- default is best rn
+vim.cmd 'colorscheme default'
+-- slight edits
+vim.cmd 'highlight WinSeparator guifg=Black'
+vim.cmd 'highlight StatusLine guibg=Black guifg=NvimLightGray3' -- edit colourscheme
+vim.cmd 'highlight StatusLineNC guibg=NvimDarkGray3 guifg=NvimLightGray3' -- edit colourscheme
