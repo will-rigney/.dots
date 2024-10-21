3afce2992937a897e631451ebf2a97e489d21494
will <william@rigney.com.au>
2024-10-21 12:49:45 +1100

    add surround plugin

---
+++ .config/nvim/lua/plugins/surround.lua
@@ -0,0 +1,12 @@
+-- surround.nvim 
+-- https://github.com/kylechui/nvim-surround
+return {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+            -- configuration here, or leave empty to use defaults
+        })
+    end
+}
